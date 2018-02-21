d8c3e741cb288246fc35412dabca8028dcd6f4f0
Noah Rosamilia <ivoahivoah@gmail.com>
2018-02-20 19:48:02 -0500

    Add purge.py

---
+++ purge.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.6
+
+import json
+import discord
+
+with open('tokens.json') as f:
+    TOKEN = json.load(f)['mr-mini']
+
+class MrMini(discord.Client):
+    async def on_ready(self):
+        self.roles = {r.name: r for r in list(self.servers)[0].roles}
+
+        for member in list(self.servers)[0].members:
+            await self.remove_roles(member, self.roles['Timeout of Shame'], self.roles['Kinda timeout but not really'])
+
+        await self.logout()
+
+mr_mini = MrMini()
+mr_mini.run(TOKEN)
