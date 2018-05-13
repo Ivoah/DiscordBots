30cdc1a177a30e6fff3f1b9530f292720cbb9587
Noah Rosamilia <ivoahivoah@gmail.com>
2018-05-13 09:58:23 -0400

    Add proper attribution

--- reegee-x.py
+++ reegee-x.py
@@ -2,72 +2,7 @@ import re
 import json
 import discord
 
-'''
-async def doit(chat, match):
-    fr = match.group(1)
-    to = match.group(2)
-    to = to.replace('\\/', '/')
-    try:
-        fl = match.group(3)
-        if fl == None:
-            fl = ''
-        fl = fl[1:]
-    except IndexError:
-        fl = ''
-
-    # Build Python regex flags
-    count = 1
-    flags = 0
-    for f in fl:
-        if f == 'i':
-            flags |= re.IGNORECASE
-        elif f == 'g':
-            count = 0
-        else:
-            await chat.reply('unknown flag: {}'.format(f))
-            return
-
-    async def substitute(original, msg):
-        try:
-            s, i = re.subn(fr, to, original, count=count, flags=flags)
-            if i > 0:
-                return (await Chat.from_message(bot, msg).reply(s))['result']
-        except Exception as e:
-            await chat.reply('u dun goofed m8: ' + str(e))
-
-    # Handle replies
-    if 'reply_to_message' in chat.message:
-        # Try to find the original message text
-        message = chat.message['reply_to_message']
-        original = find_original(message)
-        if not original:
-            return
-
-        return await substitute(original, message)
-
-    else:
-        # Try matching the last few messages
-        for msg in reversed(last_msgs[chat.id]):
-            original = find_original(msg)
-            if not original:
-                continue
-
-            return await substitute(original, msg)
-
-
-@bot.command(r'^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)(/.*)?')
-async def test(chat, match):
-    msg = await doit(chat, match)
-    if msg:
-        last_msgs[chat.id].append(msg)
-    pprint(last_msgs[chat.id])
-
-
-@bot.command(r'(.*)')
-@bot.handle('photo')
-async def msg(chat, match):
-    last_msgs[chat.id].append(chat.message)
-'''
+# Thanks to Sijmen Schoon for the original regexbot.py code https://github.com/SijmenSchoon/regexbot
 
 with open('tokens.json') as f:
     TOKEN = json.load(f)['reegee-x']
