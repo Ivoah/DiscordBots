23f5b727b1995ebebeecce1f5eaf5cbc85257532
Noah Rosamilia <ivoahivoah@gmail.com>
2018-06-11 23:07:32 -0400

    Use dict instead of list for comics

--- xkcd_update.py
+++ xkcd_update.py
@@ -3,16 +3,14 @@
 import json
 import requests
 
-comics = []
+comics = {}
 
 latest = requests.get('http://xkcd.com/info.0.json').json()['num']
 
 for i in range(1, latest + 1):
     print(f'\rUpdating xkcd database: {i}/{latest} ({i/latest*100:.2f}%)', end='')
-    if i == 404:
-        comics.append(None)
-    else:
-        comics.append(requests.get(f'http://xkcd.com/{i}/info.0.json').json())
+    if i == 404: continue
+    comics[i] = requests.get(f'http://xkcd.com/{i}/info.0.json').json()
 print()
 
 with open('xkcd.json', 'w') as f:
