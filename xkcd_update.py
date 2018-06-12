c113af24c8ae853d54c338daf6e84234c88d97d8
Noah Rosamilia <ivoahivoah@gmail.com>
2018-06-11 22:58:57 -0400

    Fix comic 404 handling and write to json file

--- xkcd_update.py
+++ xkcd_update.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 
+import json
 import requests
 
 comics = []
@@ -7,7 +8,12 @@ comics = []
 latest = requests.get('http://xkcd.com/info.0.json').json()['num']
 
 for i in range(1, latest + 1):
-    if i == 404: comics.append(None)
     print(f'\rUpdating xkcd database: {i}/{latest} ({i/latest*100:.2f}%)', end='')
-    comics.append(requests.get(f'http://xkcd.com/{i}/info.0.json').json())
+    if i == 404:
+        comics.append(None)
+    else:
+        comics.append(requests.get(f'http://xkcd.com/{i}/info.0.json').json())
 print()
+
+with open('xkcd.json', 'w') as f:
+    json.dump(comics, f)
