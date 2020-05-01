bda3b1d7e09948e423fc2a2052f8d6620fca852e
Noah Rosamilia <ivoahivoah@gmail.com>
2020-05-01 12:09:51 -0400

    Fix purge.py

--- purge.py
+++ purge.py
@@ -14,7 +14,7 @@ class MrMini(discord.Client):
     async def on_ready(self):
         self.roles = {r.name: r for r in list(self.guilds)[0].roles}
 
-        for member in list(self.servers)[0].members:
+        for member in list(self.guilds)[0].members:
             await member.remove_roles(self.roles['Timeout of Shame'], self.roles['Kinda timeout but not really'])
 
         await self.logout()
